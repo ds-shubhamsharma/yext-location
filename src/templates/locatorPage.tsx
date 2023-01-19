0e3ce676f808cdae145a95d3815a37fb19085c34
shubhamdots <shubhamsharma0888@gmail.com>
2023-01-19 18:37:19 +0530

    create locator templete & component update

--- src/templates/locatorPage.tsx
+++ src/templates/locatorPage.tsx
@@ -83,7 +83,7 @@ const LocatorPage: Template<TemplateRenderProps> = ({ document }) => {
           <DataFetch />
 
           <div className="grid grid-cols-2 gap-x-10 gap-y-10">
-            <LocationData />
+            <LocationData/>
           </div>
         </div>
       </div>
